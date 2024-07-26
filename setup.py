be3c83a55fc49f38df3aa7af77a8209d4063e788
Kiran-TheDataExplorer <mutte.kirankumar@gmail.com>
2024-07-26 17:07:39 +0530

    visual code

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Mute Kiran Kumar",
+    author_email="mutte.kirankumar@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
